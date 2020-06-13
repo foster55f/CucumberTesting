fcda93a0b3ec2421c4cb95912ddd5ca942587775
“Foster <fosterataylor@yahoo.com>
2020-06-13 16:29:40 -0600

    Edit spacing

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,17 +1,17 @@
 module RentalAgeCheck
     def is_old_enough_to_rent_car(age)
     end
-  end
-  World RentalAgeCheck
+end
+World RentalAgeCheck
 
-  Given("age is 23") do
+Given("age is 23") do
     @age = 23
-  end
+end
     
-  When("I ask if I'm old enough to rent a car") do
+When("I ask if I'm old enough to rent a car") do
     @actual_answer = is_old_enough_to_rent_car(@age)
-    end
+end
     
-  Then("I should be told {string}") do |expected_answer|
-      expect(@actual_answer).to eq(expected_answer)
-    end
+Then("I should be told {string}") do |expected_answer|
+    expect(@actual_answer).to eq(expected_answer)
+end
